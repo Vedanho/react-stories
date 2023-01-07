2b928473f7724e3684cc1a20abe74ffb1bb14633
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2023-01-07 20:11:10 +0300

    Создал компонент, содержащий в себе линии загрузки

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import ProgressItem from "./ProgressItem";
+import styles from "./Progress.module.scss";
 
-const Progress = () => {
-  return <div>Progress</div>;
+const Progress: React.FC = () => {
+  const [progress, setProgress] = React.useState(0);
+
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  
+  const stories = [
+    {
+      img: "",
+    },
+    {
+      img: "",
+    },
+    {
+      img: "",
+    },
+    {
+      img: "",
+    },
+  ];
+
+
+  return (
+    <div className={styles.line_container}>
+      {stories.map((el, index) => (
+        <ProgressItem
+          key={index}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+          index={index}
+        />
+      ))}
+    </div>
+  );
 };
 
-export default Progress;
+export default Progress
