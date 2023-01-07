a7683d046c8fa2f29a2d661d5328903533613e1e
Inasalamov Amirkhan <104070261+Vedanho@users.noreply.github.com>
2023-01-07 20:11:39 +0300

    Добавил компонент, отображающий каждую линию загрузки отдельно

--- src/components/Progress/ProgressItem.tsx
+++ src/components/Progress/ProgressItem.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "./Progress.module.scss";
 
-const ProgressItem = () => {
-  return <div>ProgressItem</div>;
+type ProgressItemProps = {
+  activeIndex: number;
+  setActiveIndex: (activeIndex: number) => void;
+  index: number;
+};
+
+const ProgressItem: React.FC<ProgressItemProps> = ({
+  activeIndex,
+  setActiveIndex,
+  index,
+}) => {
+
+  React.useEffect(() => {
+    setActiveIndex(index);
+  }, [activeIndex]);
+
+  return (
+    <div className={styles.line_item_wrapper}>
+      <div
+        className={index === activeIndex ? styles.line_item : "undefined"}
+      ></div>
+    </div>
+  );
 };
 
 export default ProgressItem;
